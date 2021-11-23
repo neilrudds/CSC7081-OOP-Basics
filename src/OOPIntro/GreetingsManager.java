cfbd3c12fbd9166b87eaa4e949019c21ec89a220
neilrudds <neilrudds@hotmail.com>
2021-11-23 19:06:48 +0000

    Initial Commit

---
+++ src/OOPIntro/GreetingsManager.java
@@ -0,0 +1,10 @@
+package OOPIntro;
+
+public class GreetingsManager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/OOPIntro/MessageCentre.java
@@ -0,0 +1,16 @@
+package OOPIntro;
+
+/**
+ * Class to respond with appropriate messages on method call
+ * @author neilr
+ *
+ */
+public class MessageCentre {
+	
+	/**
+	 * Method that prints to screen a welcome message
+	 */
+	public void displayWelcomeMessage() {
+		System.out.println("Hello and welcome");
+	}
+}
